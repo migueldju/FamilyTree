9f9b9c08a7fcf3abf7e86118e45dac90bf68c229
migueldju <migueldjur@gmail.com>
2024-11-13 19:51:40 +0100

    cŕeación repo

---
+++ FamilyTreeTest/PersonaTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FamilyTreeTest
+{
+    [TestClass]
+    public class PersonaTest
+    {
+        [TestMethod]
+        public void TestPersona()
+        {
+            Persona p = new Persona(001, "Antonio", "Rodríguez", "López", )
+        }
+    }
+}
---
+++ FamilyTreeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FamilyTreeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FamilyTreeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9cc280ea-d652-494f-b257-856ac0533604")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
